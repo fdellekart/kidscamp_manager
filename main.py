fd6ad2641c4894fd4ca4c03838b417f8b2c797d1
FlorianDellekartFHJ <florian.dellekart@edu.fh-joanneum.at>
2020-08-15 18:16:37 +0200

    Some more DO testin.

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/kids/{first_name}")
-async def root(first_name: str):
-    return {"first_name" : first_name}
+@app.get("/")
+async def root():
+    return {"message" : "Succesfully connected."}
