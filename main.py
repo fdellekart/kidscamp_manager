59217808cea0dbe6d4752aebc4040ef0d5c6532a
FlorianDellekartFHJ <florian.dellekart@edu.fh-joanneum.at>
2020-08-16 13:56:47 +0200

    Removed item class, was not needed due to spacings in keys from DF

--- main.py
+++ main.py
@@ -9,20 +9,6 @@ from datetime import date
 app = FastAPI()
 
 
-class Item(BaseModel):
-    Erziehungsberechtigter: str
-    mail_addr: str
-    telephone: str
-    Kind1: str
-    kid1_bday: str
-    Kind2: Optional[str]
-    kid2_bday: Optional[str]
-    Kind3: Optional[str]
-    kid3_bday: Optional[str]
-    Kind4: Optional[str]
-    kid4_bday: Optional[str]
-
-
 @app.get("/")
 async def root():
     return {"message" : "Succesfully connected."}
