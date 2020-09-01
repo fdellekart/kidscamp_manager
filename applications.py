eb38230d057b935624a47403be134a60fa08f001
FlorianDellekartFHJ <florian.dellekart@edu.fh-joanneum.at>
2020-09-01 15:59:30 +0200

    Added quoutes to keys

--- applications.py
+++ applications.py
@@ -17,15 +17,15 @@ def resolve_application(data: dict):
                             kids is list of dicts with keys name, birthday
     """
     parent = {
-        name: data[env["parent_name"]],
-        mail: data[env["mail"]],
-        telephone: data[env["telephone"]],
+        "name": data[env["parent_name"]],
+        "mail": data[env["mail"]],
+        "telephone": data[env["telephone"]],
     }
     kids = []
     for i in range(env["nr_of_kids"]):
         kid = {
-            name: data[env[f"kid_{i}"]], 
-            birthday: data[env[f"birthday_{i}"]]
+            "name": data[env[f"kid_{i}"]], 
+            "birthday": data[env[f"birthday_{i}"]]
         }
         if kid["name"]:
             kids.append(kid)
