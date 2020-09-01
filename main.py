9ef87f200698c90cc2ea81dbf75fb3914008e2ae
FlorianDellekartFHJ <florian.dellekart@edu.fh-joanneum.at>
2020-09-01 17:38:08 +0200

    Login Route

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from typing import Optional
 
 
 from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
 from pydantic import BaseModel
 from datetime import date
 
@@ -17,8 +18,16 @@ async def root():
     return {"message": "Succesfully connected."}
 
 
+@app.get("/login/", response_class=HTMLResponse)
+async def read_items():
+    with open("index.html", 'r') as f:
+        html_page = f.read()
+    return html_page
+
+
 @app.post("/newapplication/")
 async def new_application(request: Request):
     request_body = await request.json()
     parent, kids = resolve_application(request_body)
     add_applications(parent, kids)
+
