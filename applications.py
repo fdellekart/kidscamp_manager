0733de69715111578cbcfab196dbadb32f2d7849
FlorianDellekartFHJ <florian.dellekart@edu.fh-joanneum.at>
2020-09-01 16:34:55 +0200

    Fixed os path isfile

--- applications.py
+++ applications.py
@@ -37,7 +37,7 @@ def resolve_application(data: dict):
 def add_applications(parent: dict, kids: List[dict]):
     path = f"{env['data_directory']}/{env['data_file']}"
     df_columns = ["name", "birthday", "parent_name", "mail", "telephone"]
-    if os.isfile(path):
+    if os.path.isfile(path):
         data = pd.read_csv(path)
     else:
         data = pd.DataFrame(df_columns)
