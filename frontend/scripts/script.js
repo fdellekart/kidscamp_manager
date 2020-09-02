232ff37bf678edfa6104ec64a5de48e97f6605fa
Julia <julia.putz@fh-joanneum.at>
2020-09-02 19:02:51 +0200

    change script

--- frontend/scripts/script.js
+++ frontend/scripts/script.js
@@ -18,8 +18,8 @@ var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200){
         alert(this.responseText);
-    } else {
-        alert("Log in fehlgeschlagen"+ this.status);
+    } else if (this.readyState == 4) {
+        alert("Log in fehlgeschlagen "+ this.status);
     }
 }
 function sendRequest(){
