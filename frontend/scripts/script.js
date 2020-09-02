21775796a13bc2abbe1c6134148a3f2a0e59d754
Julia <julia.putz@fh-joanneum.at>
2020-09-02 17:34:56 +0200

    create second page overview

--- frontend/scripts/script.js
+++ frontend/scripts/script.js
@@ -7,8 +7,24 @@ function clearForm(){
 var login_btn = document.getElementById("login");
 
 login_btn.addEventListener("click", () => {
+
     clearForm();
 });
+
+
+//login
+function validate(){
+    var benutzername = document.getElementById("name").value;
+    var password = document.getElementById("pwd").value;
+
+    if (benutzername === "flotschi" && password === "12345"){
+        window.location = "./overview.html";
+        return false;
+    } else {
+        alert("Login fehlgeschlagen");
+        return false;
+    }
+}
 
 
 
