2f408155354b95bc0859f9df76b3e1954276b270
FlorianDellekartFHJ <florian.dellekart@edu.fh-joanneum.at>
2020-09-02 19:12:57 +0200

    Ready state 4

--- frontend/scripts/script.js
+++ frontend/scripts/script.js
@@ -17,7 +17,7 @@ var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200){
         alert(this.responseText);
-    } else {
+    } else if (this.readyState == 4) {
         alert("Log in fehlgeschlagen "+ this.readyState);
     }
 }
