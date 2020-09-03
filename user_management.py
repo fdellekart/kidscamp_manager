5bdef12e988923400780dc542b902f531d389103
FlorianDellekartFHJ <florian.dellekart@edu.fh-joanneum.at>
2020-09-03 21:02:55 +0200

    Tryout cookie validation

--- user_management.py
+++ user_management.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 
 
 import pandas as pd
@@ -9,7 +10,7 @@ env = EnvYAML()
 
 
 def add_user(username: str, password: str):
-    new_user = pd.DataFrame({"username" : username, "password" : password}, index=[1,])
+    new_user = pd.DataFrame({"username" : username, "password" : password, "access_token" : None, "expires" : None}, index=[0,])
     if os.path.exists(env["users_file"]):
         users = pd.read_csv(env["users_file"])
         if username in users["username"].values:
@@ -20,3 +21,11 @@ def add_user(username: str, password: str):
     else:
         new_user.to_csv(env["users_file"], index=False)
         return True
+
+
+def set_access_token(username: str, token: str, expires: datetime):
+    users = pd.read_csv(env["users_file"])
+    condition = users["username"] == username
+    users.loc[condition, "access_token"] = token
+    users.loc[condition, "expires"] = expires
+    users.to_csv(env["users_file"])
