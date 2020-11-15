5c9413f47506303b7b18239c0024b5bf5e9842bb
FlorianDellekartFHJ <florian.dellekart@edu.fh-joanneum.at>
2020-11-15 19:47:58 +0100

    fixed unindent level stuff

--- main.py
+++ main.py
@@ -114,8 +114,8 @@ async def read_users_me(current_user: User = Depends(get_current_user)):
 
 @app.post('/auth/token')
 async def login(form_data: OAuth2PasswordRequestForm = Depends()):
-   user_dict = fake_users_db.get(form_data.username)
-   if not user_dict:
+    user_dict = fake_users_db.get(form_data.username)
+    if not user_dict:
         raise HTTPException(status_code=400, detail="Incorrect username or password!")
     user = UserInDB(**user_dict)
     hashed_password = fake_hash_password(form_data.password)
