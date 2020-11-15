cfc8852794da4c7d15f90cad2586bde017d40e21
FlorianDellekartFHJ <florian.dellekart@edu.fh-joanneum.at>
2020-11-15 17:58:25 +0100

    Auth tryout

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from typing import Optional
 from datetime import date
 
 
-from fastapi import FastAPI, Request, Response, Depends, Cookie
+from fastapi import FastAPI, Request, Response, Depends, Cookie, HTTPException, status
 from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.staticfiles import StaticFiles
-from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from fastapi_login.exceptions import InvalidCredentialsException
 from fastapi_login import LoginManager
 from pydantic import BaseModel
@@ -21,7 +21,27 @@ env = EnvYAML()
 SECRET = env["secret"]
 print(SECRET)
 
-manager = LoginManager(SECRET, tokenUrl="/auth/token", use_cookie=True)
+def fake_hash_password(password: str):
+    return "fakehashed" + password
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/token")
+
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "fakehashedsecret",
+        "disabled": False,
+    },
+    "alice": {
+        "username": "alice",
+        "full_name": "Alice Wonderson",
+        "email": "alice@example.com",
+        "hashed_password": "fakehashedsecret2",
+        "disabled": True,
+    },
+}
 
 app = FastAPI()
 
@@ -29,6 +49,20 @@ app.mount("/pictures", StaticFiles(directory="frontend/pictures"), name="picture
 app.mount("/scripts", StaticFiles(directory="frontend/scripts"), name="scripts")
 app.mount("/style", StaticFiles(directory="frontend/style"), name="style")
 
+
+class User(BaseModel):
+    username: str
+    email: Optional[str] = None
+    full_name: Optional[str] = None
+    disabled: Optional[bool] = None
+
+class UserInDB(User):
+    hashed_password: str
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
 
 @app.get("/")
 async def root():
@@ -43,7 +77,7 @@ async def login_page():
 
 
 @app.get("/overview/")
-async def overview_page(user=Depends(manager)):
+async def overview_page(token: str = Depends(oauth2_scheme)):
     with open("frontend/overview.html", 'r') as f:
         html_page = f.read()
     return html_page
@@ -55,32 +89,37 @@ async def new_application(request: Request):
     parent, kids = resolve_application(request_body)
     add_applications(parent, kids)
 
+def fake_decode_token(token):
+    user = get_user(fake_users_db, token)
+    return user
 
-@manager.user_loader
-def load_user(username: str):
-    users = pd.read_csv(env["users_file"])
-    if username not in users["username"].values:
-        return False
-    else:
-        condition = users["username"] == username
-        password = users.loc[condition, "password"].values[0]
-        return {"username" : username, "password" : password}
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    user = fake_decode_token(token)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+    )
+    return user
 
+async def get_current_active_user(current_user: User = Depends(get_current_user))
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+@app.get("/users/me")
+async def read_users_me(current_user: User = Depends(get_current_user)):
+    return current_user
 
 @app.post('/auth/token')
-async def login(request: Request):
-    body = await request.json()
-    print(body)
-    username = body["username"]
-    password = body["password"]
+async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+   user_dict = fake_users_db.get(form_data.username)
+   if not user_dict:
+       raise HTTPException(status_code=400, detail="Incorrect username or password!")
+    user = UserInDB(**user_dict)
+    hashed_password = fake_hash_password(form_data.password)
+    if not hashed_password == user.hashed_password:
+        raise HTTPException(status_code=400, detail="Incorrect username or password!")
 
-    user = load_user(username)
-    if not user:
-        raise InvalidCredentialsException
-    elif password != user["password"]:
-        raise InvalidCredentialsException
-
-    access_token = manager.create_access_token(
-        data=dict(sub=username)
-    )
-    return {"access_token" : access_token, "token_type" : "bearer"}
+    return {"access_token": user.username, "token_type": "bearer"}
