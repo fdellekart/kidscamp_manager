74da4d61e4ba741acebf09869ad2c8c7f91b146d
FlorianDellekartFHJ <florian.dellekart@edu.fh-joanneum.at>
2020-11-21 23:15:03 +0100

    Changed localhost:80 to anmeldung.kiscamp.at

--- frontend/scripts/overview.js
+++ frontend/scripts/overview.js
@@ -1,20 +1,20 @@
 function checkAuth() {
     token = sessionStorage.getItem("kc_manager_token");
     if(token == null) {
-        window.location.assign("http://localhost:80/login/");
+        window.location.assign("http://anmeldung.kidscamp.at/login/");
     }
 }
 
 function getAllKidsData() {
     var xhttp = new XMLHttpRequest();
-    xhttp.open("GET", "http://localhost:80/allapplications/");
+    xhttp.open("GET", "http://anmeldung.kidscamp.at/allapplications/");
     xhttp.onreadystatechange = function () {
         if(xhttp.readyState === XMLHttpRequest.DONE && xhttp.status === 200) {
             console.log(xhttp.response)
             return JSON.parse(xhttp.response)
         } else if (xhttp.readyState === XMLHttpRequest.DONE && xhttp.status === 401) {
             console.log("Not authorized!")
-            window.location.assign("http://localhost:80/login/")
+            window.location.assign("http://anmeldung.kidscamp.at/login/")
         }
     }
     xhttp.setRequestHeader("authorization", "Bearer " + sessionStorage.getItem("kc_manager_token"))
