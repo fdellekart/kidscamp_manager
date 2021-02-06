cfd748d5f5bebc76c027b12bdc315f5260bde854
FlorianDellekartFHJ <florian.dellekart@edu.fh-joanneum.at>
2021-02-06 21:51:05 +0100

    Docstrings

--- applications.py
+++ applications.py
@@ -16,6 +16,19 @@ env = EnvYAML()
 
 
 def add_applications(parent: tuple, kids: List[Tuple], db_conn):
+    """Add parent to parents table and kids to kids table
+
+    :param parent: (parent_id: int,
+                    first_name: str,
+                    last_name: str,
+                    e_mail: str,
+                    telephone: str)
+    :param kids: list of tuples in form (kid_id: int,
+                                         parent: int (id in parents table),
+                                         first_name: str,
+                                         last_name: str)
+    :param db_conn: sqlite db connection
+    """
     cursor = db_conn.cursor()
 
     cursor.execute(create_parent_query)
@@ -27,6 +40,14 @@ def add_applications(parent: tuple, kids: List[Tuple], db_conn):
 
 
 def get_all_kids(db_conn):
+    """Get all kids in kids table
+
+    :param db_conn: sqlite db connection
+    :return: list of tuples in form (kid_id: int,
+                                     parent: int (id in parents table),
+                                     first_name: str,
+                                     last_name: str)
+    """
     cursor = db_conn.cursor()
     cursor.execute("SELECT * FROM kids")
     return cursor.fetchall()
