e61e0b3399aa083b04f825ad473b65e6c211ac8a
FlorianDellekartFHJ <florian.dellekart@edu.fh-joanneum.at>
2021-02-26 23:04:20 +0100

    Add Kid model

--- models/applications.py
+++ models/applications.py
@@ -1,9 +1,18 @@
 from typing import List
+from datetime import date
 
 
 from pydantic import BaseModel
 
 
+class Kid(BaseModel):
+    id: int
+    parent_id: int
+    first_name: str
+    last_name: str
+    birthday: date
+
+
 class Application(BaseModel):
-    kids: List[list]
+    kids: List[Kid]
     parent: list
