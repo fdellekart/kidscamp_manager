629b4d1f1b19538f73abdbb853a459438f3f89a7
fdellekart <florian.dellekart@edu.fh-joanneum.at>
2021-02-27 23:08:53 +0100

    Add functions relevant for auth to auth module

---
+++ auth/__init__.py
@@ -0,0 +1,47 @@
+from datetime import datetime, timedelta
+from typing import Optional
+
+
+from fastapi.security import OAuth2PasswordBearer
+from passlib.context import CryptContext
+from envyaml import EnvYAML
+from jose import jwt
+
+
+from database.users import get_user
+
+
+env = EnvYAML()
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/token")
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password: str):
+    return pwd_context.hash(password)
+
+
+def authenticate_user(username: str, password: str):
+    user = get_user(username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, env["secret"], "HS256")
+    return encoded_jwt
