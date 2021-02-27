d7c1493d506aa9da2ab515c82386c07b71475903
fdellekart <florian.dellekart@edu.fh-joanneum.at>
2021-02-27 23:10:31 +0100

    Move all routes to subfolder

--- main.py
+++ main.py
@@ -1,39 +1,13 @@
-from datetime import timedelta
-from typing import List
 import sqlite3
 
 
-from fastapi import FastAPI, Depends, HTTPException, status
-from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from envyaml import EnvYAML
-from jose import JWTError, jwt
+from fastapi import FastAPI
 
 
-from database import add_applications, get_all_kids, get_all_parents
-from user_management import (
-    get_user,
-    authenticate_user,
-    create_access_token,
-    User,
-    TokenData,
-    Token,
-)
 from models.applications import Kid, Parent
 from exceptions import credentials_exception, unauthorized_exception
-from views import applications
+from views import applications, auth, users
 
-env = EnvYAML()
-
-SECRET = env["secret"]
-EXPIRE_MINUTES = 5
-
-def fake_hash_password(password: str):
-    return "fakehashed" + password
-
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/token")
 
 app = FastAPI()
 
@@ -48,74 +22,4 @@ async def shutdown():
 
 
 app.include_router(applications.router)
-
-
-@app.get("/", response_class=HTMLResponse)
-async def root():
-    with open("frontend/index.html", "r") as f:
-        html_page = f.read()
-    return html_page
-
-
-@app.get("/login/", response_class=HTMLResponse)
-async def login_page():
-    with open("frontend/login.html", "r") as f:
-        html_page = f.read()
-    return html_page
-
-
-@app.get("/overview/", response_class=HTMLResponse)
-async def overview_page():
-    with open("frontend/overview.html", "r") as f:
-        html_page = f.read()
-    return html_page
-
-
-@app.get("/allkids/")
-async def all_kids(token: str = Depends(oauth2_scheme)):
-    try:
-        payload = jwt.decode(token, SECRET, algorithms=["HS256"])
-        return get_all_kids(app.state.db_conn)
-    except JWTError:
-        raise credentials_exception
-
-
-@app.get("/allparents/")
-async def parent(token: str = Depends(oauth2_scheme)):
-    try:
-        payload = jwt.decode(token, SECRET, algorithms=["HS256"])
-        return get_all_parents(app.state.db_conn)
-    except JWTError:
-        raise credentials_exception
-
-
-async def get_current_user(token: str = Depends(oauth2_scheme)):
-    try:
-        payload = jwt.decode(token, SECRET, algorithms=["HS256"])
-        username: str = payload.get("sub")
-        if username is None:
-            raise credentials_exception
-        token_data = TokenData(username=username)
-    except JWTError:
-        raise credentials_exception
-    user = get_user(username=token_data.username)
-    if user is None:
-        raise credentials_exception
-    return user
-
-
-@app.get("/users/me")
-async def read_users_me(current_user: User = Depends(get_current_user)):
-    return current_user
-
-
-@app.post("/auth/token", response_model=Token)
-async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
-    user = authenticate_user(form_data.username, form_data.password)
-    if not user:
-        raise unauthorized_exception
-    access_token_expires = timedelta(minutes=EXPIRE_MINUTES)
-    access_token = create_access_token(
-        data={"sub": user.username}, expires_delta=access_token_expires
-    )
-    return {"access_token": access_token, "token_type": "bearer"}
+app.include_router(auth.router)
--- views/applications.py
+++ views/applications.py
@@ -1,18 +1,45 @@
 from typing import List
 
 
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Depends
+from jose import jwt, JWTError
+from envyaml import EnvYAML
 
 
 from models.applications import Application
-from database import add_applications
+from database.applications import add_applications
+from database.kids import get_all_kids
+from database.parents import get_all_parents
+from auth import oauth2_scheme
+from exceptions import credentials_exception
+
+env = EnvYAML()
+
+SECRET = env["secret"]
 
 
 router = APIRouter()
-
 
 @router.post("/newapplication/")
 async def new_application(request: Request):
     body = await request.json()
     application = Application(**body)
     add_applications(application.parent, application.kids, request.app.state.db_conn)
+
+
+@router.get("/allkids/")
+async def all_kids(token: str = Depends(oauth2_scheme)):
+    try:
+        payload = jwt.decode(token, SECRET, algorithms=["HS256"])
+        return get_all_kids(token.state.db_conn)
+    except JWTError:
+        raise credentials_exception
+
+
+@router.get("/allparents/")
+async def parent(token: str = Depends(oauth2_scheme)):
+    try:
+        payload = jwt.decode(token, SECRET, algorithms=["HS256"])
+        return get_all_parents(token.state.db_conn)
+    except JWTError:
+        raise credentials_exception
