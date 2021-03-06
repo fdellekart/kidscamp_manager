f98ddb6814fd824cb50e64a91440487799d9ea72
fdellekart <florian.dellekart@hotmail.de>
2021-03-06 16:47:05 +0100

    Use Depends(get_db_conn), NOT WORKING!!

--- views/auth.py
+++ views/auth.py
@@ -1,13 +1,15 @@
 from datetime import timedelta
 
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Request
 from fastapi.security import OAuth2PasswordRequestForm
 
 
 from models.auth import Token
 from auth import authenticate_user, create_access_token
 from exceptions import unauthorized_exception
+from database import get_db_conn
+
 
 EXPIRE_MINUTES = 30
 
@@ -15,8 +17,8 @@ router = APIRouter()
 
 
 @router.post("/auth/token", response_model=Token)
-async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
-    user = authenticate_user(form_data.username, form_data.password)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db_conn = Depends(get_db_conn)):
+    user = authenticate_user(form_data.username, form_data.password, db_conn)
     if not user:
         raise unauthorized_exception
     access_token_expires = timedelta(minutes=EXPIRE_MINUTES)
