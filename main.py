531b87e2ca802f357d178a59134b4eceb3246cf4
fdellekart <florian.dellekart@hotmail.de>
2021-03-07 11:23:03 +0100

    Include users router and comment events

--- main.py
+++ main.py
@@ -1,25 +1,24 @@
-import sqlite3
+# import sqlite3
 
 
 from fastapi import FastAPI
 
 
-from models.applications import Kid, Parent
-from exceptions import credentials_exception, unauthorized_exception
 from views import applications, auth, users
 
 
 app = FastAPI()
 
-@app.on_event("startup")
-async def startup():
-    app.state.db_conn = sqlite3.connect("kidscamp.db")
+# @app.on_event("startup")
+# async def startup():
+#     app.state.db_conn = sqlite3.connect("kidscamp.db")
 
 
-@app.on_event("shutdown")
-async def shutdown():
-    app.state.db_conn.close()
+# @app.on_event("shutdown")
+# async def shutdown():
+#     app.state.db_conn.close()
 
 
 app.include_router(applications.router)
 app.include_router(auth.router)
+app.include_router(users.router)
