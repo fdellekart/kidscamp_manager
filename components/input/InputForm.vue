20f22bb1081b8964619f5de38ccfb31b1a8b74cb
fdellekart <florian.dellekart@hotmail.de>
2021-07-05 19:20:16 +0200

    Add component for input

---
+++ components/input/InputForm.vue
@@ -0,0 +1,47 @@
+<template>
+  <form @submit.prevent="onSubmit">
+    <AppControlInput v-model="personData.firstName">Vorname</AppControlInput>
+    <AppControlInput v-model="personData.lastName">Nachname</AppControlInput>
+    <p v-if="showWarning" class="warning">Bitte Daten vollständig angeben!</p>
+    <AppButton type="submit">Speichern</AppButton>
+  </form>
+</template>
+
+<script>
+export default {
+  props: {
+    person: {
+      type: Object,
+      default: undefined,
+    },
+  },
+  data() {
+    return {
+      personData: {
+        firstName: this.person ? this.person.firstName : '',
+        lastName: this.person ? this.person.lastName : '',
+      },
+      showWarning: false,
+    }
+  },
+  methods: {
+    onSubmit() {
+      const isFirstNameShort = this.personData.firstName.length < 2
+      const isLastNameShort = this.personData.lastName.length < 2
+      if (isFirstNameShort || isLastNameShort) {
+        this.showWarning = true
+        return
+      }
+      this.showWarning = false
+      this.$emit('save', this.personData)
+    },
+  },
+}
+</script>
+
+<style scoped>
+.warning {
+  color: red;
+  font-weight: bold;
+}
+</style>
