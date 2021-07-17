893da331d6cb1595513c19fa31047e664a099433
fdellekart <florian.dellekart@hotmail.de>
2021-07-17 16:01:56 +0200

    Add login page from other project

---
+++ pages/login/index.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="admin-auth-page">
+    <div class="auth-container">
+      <form @submit.prevent="onSubmit">
+        <p
+          v-if="showWarning & isLogin"
+          class="unauthenticated-warn"
+          style="color: red"
+        >
+          Login not Possible
+        </p>
+        <AppControlInput v-model="email" type="email"
+          >E-Mail Address</AppControlInput
+        >
+        <AppControlInput v-model="password" type="password"
+          >Password</AppControlInput
+        >
+        <AppButton type="submit">{{ isLogin ? 'Login' : 'Sign Up' }}</AppButton>
+        <AppButton
+          type="button"
+          btn-style="inverted"
+          style="margin-left: 10px"
+          @click="isLogin = !isLogin"
+          >Switch to {{ isLogin ? 'Signup' : 'Login' }}</AppButton
+        >
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AdminAuthPage',
+  layout: 'admin',
+  data() {
+    return {
+      isLogin: true,
+      email: '',
+      password: '',
+      showWarning: false,
+    }
+  },
+  methods: {
+    onSubmit() {
+      this.$store
+        .dispatch('authenticateUser', {
+          isLogin: this.isLogin,
+          email: this.email,
+          password: this.password,
+        })
+        .then(() => this.$router.push('/admin'))
+        .catch((e) => {
+          console.log(e)
+          this.showWarning = true
+        })
+    },
+  },
+}
+</script>
+
+<style scoped>
+.admin-auth-page {
+  padding: 20px;
+}
+
+.auth-container {
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  box-shadow: 0 2px 2px #ccc;
+  width: 300px;
+  margin: auto;
+  padding: 10px;
+  box-sizing: border-box;
+}
+</style>
