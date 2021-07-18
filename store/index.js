e5086319d8df741a5673ada574f9f15b6d7429b8
fdellekart <florian.dellekart@hotmail.de>
2021-07-18 14:32:12 +0200

    Add store functions for auth from other project

--- store/index.js
+++ store/index.js
@@ -37,6 +37,56 @@ const createStore = () => {
             )
           })
       },
+      clearAuthToken(vuexContext) {
+        if (process.client) {
+          localStorage.removeItem('jwt')
+          localStorage.removeItem('expirationDate')
+        }
+        Cookie.remove('jwt')
+        Cookie.remove('expirationDate')
+        vuexContext.commit('clearAuthToken')
+      },
+      initAuth(vuexContext, req) {
+        let token
+        let expirationDate
+        if (req) {
+          if (!req.headers.cookie) {
+            return
+          }
+          const jwtCookie = req.headers.cookie
+            .split(';')
+            .find((c) => c.trim().startsWith('jwt='))
+          if (!jwtCookie) {
+            return
+          }
+          token = jwtCookie.split('=')[1]
+          const expirationCookie = req.headers.cookie
+            .split(';')
+            .find((c) => c.trim().startsWith('expirationDate='))
+          expirationDate = expirationCookie.split('=')[1]
+        } else {
+          token = localStorage.getItem('jwt')
+          console.log('Got token', token)
+          if (!token) {
+            return
+          }
+          expirationDate = localStorage.getItem('expirationDate')
+        }
+
+        if (new Date().getTime() > +expirationDate || !token) {
+          vuexContext.dispatch('clearAuthToken')
+          return
+        }
+        vuexContext.commit('setAuthToken', token)
+      },
+    },
+    getters: {
+      authToken(state) {
+        return state.authToken
+      },
+      isAuthenticated(state) {
+        return state.authToken != null
+      },
     },
   })
 }
