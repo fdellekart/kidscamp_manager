924c8bf6ebcc6e0868298b8583a64ab5db0fe1ab
fdellekart <florian.dellekart@hotmail.de>
2021-08-14 17:10:24 +0200

    Commit files for moving to another machine

--- plugins/core-components.js
+++ plugins/core-components.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
+import { BootstrapVue } from 'bootstrap-vue'
 
 import AppButton from '@/components/UI/AppButton'
 import AppControlInput from '@/components/UI/AppControlInput'
 
+Vue.use(BootstrapVue)
 Vue.component('AppButton', AppButton)
 Vue.component('AppControlInput', AppControlInput)
