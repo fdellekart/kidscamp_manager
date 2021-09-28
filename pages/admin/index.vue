7b766f5f8198c64a5307c78198c094e0bed35d6c
Florian Dellekart <florian.dellekart@hotmail.de>
2021-09-28 20:46:17 +0200

    Add table with applications to admin page

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,11 +1,49 @@
 <template>
-  <div><b-table :items="$store.getters.applicationsToDisplay"></b-table></div>
+  <div>
+    <b-table
+      striped
+      hover
+      :items="$store.getters.applicationsToDisplay"
+      :fields="tableFields"
+    ></b-table>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'AdminPage',
   middleware: ['check-auth', 'auth'],
+  data() {
+    return {
+      tableFields: [
+        {
+          key: 'firstName',
+          label: 'Vorname',
+          sortable: true,
+        },
+        {
+          key: 'lastName',
+          label: 'Nachname',
+          sortable: true,
+        },
+        {
+          key: 'age',
+          label: 'Alter',
+          sortable: true,
+        },
+        {
+          key: 'parent',
+          label: 'Erziehungsberechtigter',
+          sortable: true,
+        },
+        {
+          key: 'mail',
+          label: 'E-Mail',
+          sortable: true,
+        },
+      ],
+    }
+  },
   mounted() {
     this.$store.dispatch('fetchApplications')
   },
