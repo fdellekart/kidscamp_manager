9e80ebdcbd9b712d7cde5a24f63374a12a384872
Florian Dellekart <florian.dellekart@hotmail.de>
2021-09-29 21:07:02 +0200

    Add modal at deletion

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -2,8 +2,12 @@
   <div>
     <admin-table
       :applications="$store.getters.applicationsToDisplay"
-      @delete="deleteApplication"
+      @delete="handleDeletionClick"
     />
+    <b-modal id="warn-delete" @ok="deleteApplication"
+      >Du bist dabei {{ applicationIdToDelete }} zu löschen. Bist du dir
+      sicher?</b-modal
+    >
   </div>
 </template>
 
@@ -14,12 +18,22 @@ export default {
   name: 'AdminPage',
   components: [AdminTable],
   middleware: ['check-auth', 'auth'],
+  data() {
+    return {
+      applicationIdToDelete: null,
+    }
+  },
   mounted() {
     this.$store.dispatch('fetchApplications')
   },
   methods: {
-    deleteApplication(applicationId) {
-      this.$store.dispatch('deleteApplication', applicationId)
+    handleDeletionClick(applicationId) {
+      this.applicationIdToDelete = applicationId
+      this.$bvModal.show('warn-delete')
+    },
+    deleteApplication() {
+      this.$store.dispatch('deleteApplication', this.applicationIdToDelete)
+      this.applicationIdToDelete = null
     },
   },
 }
