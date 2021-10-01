a2bb268dcaa9624930befda6874e67d878980824
Florian Dellekart <florian.dellekart@hotmail.de>
2021-10-01 18:44:43 +0200

    Add editing stuff for other fields

--- components/AdminTable.vue
+++ components/AdminTable.vue
@@ -19,6 +19,38 @@
       ></b-form-input>
       <span v-else>{{ data.item.firstName }}</span>
     </template>
+    <template #cell(lastName)="data">
+      <b-form-input
+        v-if="isEditing(data.item.id)"
+        v-model="data.item.lastName"
+        :value="data.item.lastName"
+      ></b-form-input>
+      <span v-else>{{ data.item.lastName }}</span>
+    </template>
+    <template #cell(age)="data">
+      <b-form-input
+        v-if="isEditing(data.item.id)"
+        v-model="data.item.age"
+        :value="data.item.age"
+      ></b-form-input>
+      <span v-else>{{ data.item.age }}</span>
+    </template>
+    <template #cell(parent)="data">
+      <b-form-input
+        v-if="isEditing(data.item.id)"
+        v-model="data.item.parent"
+        :value="data.item.parent"
+      ></b-form-input>
+      <span v-else>{{ data.item.parent }}</span>
+    </template>
+    <template #cell(mail)="data">
+      <b-form-input
+        v-if="isEditing(data.item.id)"
+        v-model="data.item.mail"
+        :value="data.item.mail"
+      ></b-form-input>
+      <a v-else :href="'mailto:' + data.item.mail">{{ data.item.mail }}</a>
+    </template>
   </b-table>
 </template>
 
