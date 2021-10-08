f4d9d94ac5cafd9e9cae9b81e5e7738bd31a5ba9
Florian Dellekart <florian.dellekart@hotmail.de>
2021-10-08 18:05:20 +0200

    Add barchart to admin page

--- components/ApplicationStats.vue
+++ components/ApplicationStats.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="container">
     <span>Aktuell {{ numberApplications }} Anmeldungen</span>
+    <div class="chart-container">
+      <Apexchart
+        :series="[{ name: 'Anmeldungen', data: Object.values(ageHistogram) }]"
+        :options="{
+          chart: { type: 'bar', toolbar: { show: false }, height: '100%' },
+          xaxis: { categories: Object.keys(ageHistogram) },
+          yaxis: { show: false },
+        }"
+      ></Apexchart>
+    </div>
   </div>
 </template>
 
@@ -12,14 +22,32 @@ export default {
       required: true,
     },
   },
+  computed: {
+    ageHistogram() {
+      const applications = this.$store.getters.applications
+      const hist = {}
+      Object.values(applications).forEach((application) => {
+        if (!hist[application.child.age]) {
+          hist[application.child.age] = 0
+        }
+        hist[application.child.age] += 1
+      })
+      return hist
+    },
+  },
 }
 </script>
 
 <style scoped>
 .container {
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   margin: 15px;
+  width: 100%;
+  height: 150px;
+}
+.chart-container {
+  height: 100%;
 }
 span {
   font-size: 30px;
