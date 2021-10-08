5159ff9369881ec113c041035bba6dbf4de13423
Florian Dellekart <florian.dellekart@hotmail.de>
2021-10-08 18:04:30 +0200

    Add apexcharts to project

--- plugins/core-components.js
+++ plugins/core-components.js
@@ -4,6 +4,11 @@ import { BootstrapVue } from 'bootstrap-vue'
 import AppButton from '@/components/UI/AppButton'
 import AppControlInput from '@/components/UI/AppControlInput'
 
+import VueApexCharts from 'vue-apexcharts'
+
 Vue.use(BootstrapVue)
 Vue.component('AppButton', AppButton)
 Vue.component('AppControlInput', AppControlInput)
+
+Vue.use(VueApexCharts)
+Vue.component('Apexchart', VueApexCharts)
