2cd9dcf4af5acf987e12f25c43446a7d3de8b93d
Florian Dellekart <florian.dellekart@hotmail.de>
2021-10-09 17:45:55 +0200

    Fix weird bullshit

--- plugins/core-components.js
+++ plugins/core-components.js
@@ -1,14 +1,15 @@
 import Vue from 'vue'
 import { BootstrapVue } from 'bootstrap-vue'
 
+import VueApexCharts from 'vue-apexcharts'
 import AppButton from '@/components/UI/AppButton'
 import AppControlInput from '@/components/UI/AppControlInput'
 
-import VueApexCharts from 'vue-apexcharts'
+export default () => {
+  Vue.component('AppButton', AppButton)
+  Vue.component('AppControlInput', AppControlInput)
+  Vue.component('Apexchart', VueApexCharts)
 
-Vue.use(BootstrapVue)
-Vue.component('AppButton', AppButton)
-Vue.component('AppControlInput', AppControlInput)
-
-Vue.use(VueApexCharts)
-Vue.component('Apexchart', VueApexCharts)
+  Vue.use(VueApexCharts)
+  Vue.use(BootstrapVue)
+}
