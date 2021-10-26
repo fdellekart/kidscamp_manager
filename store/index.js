538a71c6f11806a4a25e31de01aca7621f60b15b
Florian Dellekart <florian.dellekart@hotmail.de>
2021-10-26 17:45:17 +0200

    Remove unused old auth functions from store

--- store/index.js
+++ store/index.js
@@ -1,12 +1,10 @@
 import Vuex from 'vuex'
 import Vue from 'vue'
-import Cookie from 'js-cookie'
 
 const createStore = () => {
   return new Vuex.Store({
     state: {
       user: null,
-      authToken: null,
       applications: {},
     },
     mutations: {
@@ -81,76 +79,8 @@ const createStore = () => {
           vuexContext.commit('setApplications', snapshot.val())
         })
       },
-      authenticateUser(vuexContext, userData) {
-        return this.$axios
-          .post('/auth/', {
-            email: userData.email,
-            password: userData.password,
-            returnSecureToken: true,
-          })
-          .then((res) => {
-            vuexContext.commit('setAuthToken', res.data.idToken)
-            localStorage.setItem('jwt', res.data.idToken)
-            Cookie.set('jwt', res.data.idToken)
-            localStorage.setItem(
-              'expirationDate',
-              new Date().getTime() + +res.data.expiresIn * 1000
-            )
-            Cookie.set(
-              'expirationDate',
-              new Date().getTime() + +res.data.expiresIn * 1000
-            )
-          })
-      },
-      clearAuthToken(vuexContext) {
-        if (process.client) {
-          localStorage.removeItem('jwt')
-          localStorage.removeItem('expirationDate')
-        }
-        Cookie.remove('jwt')
-        Cookie.remove('expirationDate')
-        vuexContext.commit('clearAuthToken')
-      },
-      initAuth(vuexContext, req) {
-        let token
-        let expirationDate
-        if (req) {
-          if (!req.headers.cookie) {
-            return
-          }
-          const jwtCookie = req.headers.cookie
-            .split(';')
-            .find((c) => c.trim().startsWith('jwt='))
-          if (!jwtCookie) {
-            return
-          }
-          token = jwtCookie.split('=')[1]
-          const expirationCookie = req.headers.cookie
-            .split(';')
-            .find((c) => c.trim().startsWith('expirationDate='))
-          expirationDate = expirationCookie.split('=')[1]
-        } else {
-          token = localStorage.getItem('jwt')
-          if (!token) {
-            return
-          }
-          expirationDate = localStorage.getItem('expirationDate')
-        }
-
-        if (new Date().getTime() > +expirationDate || !token) {
-          vuexContext.dispatch('clearAuthToken')
-          return
-        }
-        vuexContext.commit('setAuthToken', token)
-      },
     },
     getters: {
-      authToken(state) {
-        return state.authToken
-      },
-      isAuthenticated(state) {
-        return state.authToken != null
-      },
       applications(state) {
         return state.applications
       },
