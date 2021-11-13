ab9484445954cb86d4c21bef3c31d9702212efcc
Florian Dellekart <florian.dellekart@hotmail.de>
2021-11-13 15:25:02 +0100

    Remove test route and add rename users route to download

--- api/index.js
+++ api/index.js
@@ -4,12 +4,10 @@ const express = require('express')
 const app = express()
 
 // Require API routes
-const users = require('./routes/users')
-const test = require('./routes/test')
+const users = require('./routes/download')
 
 // Import API Routes
 app.use(users)
-app.use(test)
 
 // Export express app
 module.exports = app
