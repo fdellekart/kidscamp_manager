654c2bdc6ff69248b2cccc45ba712e6ad901c3cf
Florian Dellekart <florian.dellekart@hotmail.de>
2021-11-28 14:18:40 +0100

    Format and send confirmation mail html content

--- api/routes/confirmation.js
+++ api/routes/confirmation.js
@@ -1,7 +1,37 @@
+const fs = require('fs')
 const { Router } = require('express')
 const nodemailer = require('nodemailer')
 
 const router = Router()
+
+const format = function (text, args) {
+  let newText = text
+  for (const attr in args) {
+    newText = newText.split('${' + attr + '}').join(args[attr])
+  }
+  return newText
+}
+
+const ulFromChildren = function (children) {
+  const strings = children.map((child) => {
+    return (
+      '<li>' +
+      child.firstName +
+      ' ' +
+      child.lastName +
+      ', ' +
+      child.age +
+      ' Jahre</li>'
+    )
+  })
+  return '<ul>' + strings.join('\n') + '</ul>'
+}
+
+const mailTemplate = fs
+  .readFileSync(
+    '/home/florian/Documents/KidsCamp/kidscamp_manager/api/confirmation-mail-template.html'
+  )
+  .toString()
 
 const transporter = nodemailer.createTransport({
   host: 'smtp.kidscamp.at',
@@ -21,9 +51,12 @@ router.post('/application/confirm', function (req, res, next) {
     .sendMail({
       from: '"KidsCamp Anmeldung" <noreply@kidscamp.at>', // sender address
       to: req.body.mail, // list of receivers
-      subject: 'Hello ✔', // Subject line
-      text: 'Hello world?', // plain text body
-      html: '<b>Hello world?</b>', // html body
+      subject: 'Bestätigung Anmeldung KidsCamp ' + new Date().getFullYear(), // Subject line
+      text: 'Anmdeldung KidsCamp', // plain text body
+      html: format(mailTemplate, {
+        name: req.body.firstName,
+        children: ulFromChildren(req.body.children),
+      }), // html body
     })
     .then((x) => console.log(x))
     .catch((e) => console.log('Error:', e))
