df34e3ec470078bde23e04816a808ec08ff0b644
Florian Dellekart <florian.dellekart@hotmail.de>
2022-01-17 17:55:47 +0100

    Configure and authenticate to firebase in servermiddleware

--- api/index.js
+++ api/index.js
@@ -1,4 +1,28 @@
 const express = require('express')
+const firebase = require('firebase')
+
+const firebaseConfig = {
+  apiKey: process.env.AUTH_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+}
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
+
+firebase // Authenticate for firebase
+  .auth()
+  .signInWithEmailAndPassword(
+    process.env.FIREBASE_USER,
+    process.env.FIREBASE_PW
+  )
+  .then((user) => {
+    console.log('Logged into firbase successfully!')
+  })
 
 // Create express instance
 const app = express()
